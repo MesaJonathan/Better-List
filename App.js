daabfc97ba3d2831cae87b72bc3f8f9b96511b80
MesaJonathan <72950525+MesaJonathan@users.noreply.github.com>
2022-02-12 18:06:23 -0500

    configuration and debug stuff

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Beginning execution...");
+  let x = 1;
+  
   return (
     <View style={styles.container}>
-      <Text>penis </Text>
+      <Text> penis </Text>
       <StatusBar style="auto" />
     </View>
   );
